35d8ab218b087398e7a542d256d34b7e7de6f73a
Blake Wu <bwu@mozilla.com>
2014-08-28 17:11:30 +0800

    Bug 1009410 - Expose graphic Buffer to MediaCodecBug. r=sotaro
    Conflicts:
    	include/media/stagefright/ACodec.h
    	media/libstagefright/ACodec.cpp
    	media/libstagefright/MediaCodec.cpp

--- include/media/stagefright/MediaCodec.h
+++ include/media/stagefright/MediaCodec.h
@@ -26,6 +26,7 @@
 namespace android {
 
 struct ABuffer;
+struct ACodec;
 struct AMessage;
 struct AString;
 struct CodecBase;
@@ -143,6 +144,10 @@ struct MediaCodec : public AHandler {
 
     status_t setParameters(const sp<AMessage> &params);
 
+    // This is used to get graphicBuffer with the index obtained from
+    // calling dequeueOutputBuffer()
+    sp<GraphicBuffer> getOutputGraphicBufferFromIndex(size_t index);
+
 protected:
     virtual ~MediaCodec();
     virtual void onMessageReceived(const sp<AMessage> &msg);
@@ -220,7 +225,7 @@ private:
     State mState;
     sp<ALooper> mLooper;
     sp<ALooper> mCodecLooper;
-    sp<CodecBase> mCodec;
+    sp<ACodec> mCodec;
     AString mComponentName;
     uint32_t mReplyID;
     uint32_t mFlags;
